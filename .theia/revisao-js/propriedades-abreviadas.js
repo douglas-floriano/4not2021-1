88aaae7667b8a2eef482c556cc5f4abde002ad96
douglas-floriano <doudouglasglascosta@gmail.com>
2021-02-10 00:52:43 +0000

    m

---
+++ .theia/revisao-js/propriedades-abreviadas.js
@@ -0,0 +1,10 @@
+//Criando um objeto cujo valor das propriedades
+//vem das variáveis
+
+let usuario = 'joao'
+let senha = "Deus123"
+
+let userInfo = {
+    login: usuario,
+    passworde: senha
+}
